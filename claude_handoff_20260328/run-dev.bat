abda3a4c9ca8a947c4b23bb36846fa51debc97a2
kohyojun0806 <kohyojun0806@gmail.com>
2026-03-29 12:36:23 +0900

    chore: remove obsolete claude handoff artifacts

--- claude_handoff_20260328/run-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-start "suneung-api" cmd /k "set SERVER_PORT=8787&&node server\index.js"
-start "suneung-web" cmd /k "set PORT=3000&&npm start"
-echo [dev] api: http://localhost:8787
-echo [dev] web: http://localhost:3000
--- claude_upload_pack/run-preview.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-set PREVIEW_PORT=3001
-echo [preview] starting at http://localhost:%PREVIEW_PORT%
-node server\preview.js
